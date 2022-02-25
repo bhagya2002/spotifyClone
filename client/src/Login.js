50732adef85609c290ba3c334bf4d8a7e9444eb5
bhagya2002 <bhagya02.p@gmail.com>
2022-02-24 23:35:59 -0700

    comments added

--- client/src/Login.js
+++ client/src/Login.js
@@ -10,6 +10,7 @@ export default function Login() {
       className="d-flex justify-content-center align-items-center"
       style={{ minHeight: "100vh" }}
     >
+      {/* spotify auth */}
       <a className="btn btn-success btn-lg" href={AUTH_URL}>
         Login With Spotify
       </a>
